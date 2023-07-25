288b30bb84e10141dee31711df1fe3fa8141f0a9
vadim-solov <92580042+vadim-solov@users.noreply.github.com>
2023-07-25 17:46:47 +0500

    - Added input service - Added composite root - Added Player.cs

---
+++ Assets/Game/Scripts/CompositeRoot/MainCompositeRoot.cs
@@ -0,0 +1,24 @@
+﻿using Game.Scripts.Services;
+using UnityEngine;
+
+namespace Game.Scripts.CompositeRoot
+{
+    public class MainCompositeRoot : MonoBehaviour
+    {
+        [SerializeField]
+        private Player.Player _player;
+
+        private IInputService _inputService;
+
+        private void Awake()
+        {
+            _inputService = new InputService();
+            _player.Init(_inputService);
+        }
+
+        private void Update()
+        {
+            _inputService.Update();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using Game.Scripts.Services;
+using UnityEngine;
+
+namespace Game.Scripts.Player
+{
+    [RequireComponent(typeof(RequireComponent))]
+    public class Player : MonoBehaviour
+    {
+        private CharacterController _characterController;
+
+        private IInputService _inputService;
+
+        public void Init(IInputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            Vector3 nexPosition = new Vector3(_inputService.Axis.x, transform.position.y, _inputService.Axis.z);
+            nexPosition.Normalize();
+            _characterController.SimpleMove(_inputService.Axis * 5000f * Time.deltaTime);
+        }
+    }
+}
