82cb5c1f8b03b951dfb91ff4eca38aef2a834840
vadim-solov <92580042+vadim-solov@users.noreply.github.com>
2023-07-26 13:55:23 +0500

    - Renamed class LevelsConfig => GameConfig - Added data in GameConfig - Added Player class

---
+++ Assets/Game/Scripts/Configs/GameConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Configs
+{
+    [CreateAssetMenu(menuName = "Configs/GameConfig", fileName = "GameConfig")]
+    public class GameConfig : ScriptableObject
+    {
+        [SerializeField]
+        private Player.Player _playerPrefab;
+        [SerializeField]
+        private List<Enemy.Enemy> _enemiesPrefabs;
+        [SerializeField]
+        private List<Level> _allLevelsPrefabs;
+
+        public Player.Player PlayerPrefab => _playerPrefab;
+        public List<Enemy.Enemy> EnemiesPrefabs => _enemiesPrefabs;
+        public List<Level> AllLevelsPrefabs => _allLevelsPrefabs;
+    }
+}
---
+++ Assets/Game/Scripts/Player/Player.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Player
+{
+    public class Player : MonoBehaviour
+    {
+    }
+}
