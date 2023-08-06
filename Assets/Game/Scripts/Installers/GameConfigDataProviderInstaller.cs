4629ff0ea9086c4626b1c0d7d3c3a2ae22794283
vadim-solov <92580042+vadim-solov@users.noreply.github.com>
2023-08-07 00:43:03 +0500

    Added Installers folder

--- Assets/Game/Scripts/Installers/GameConfigDataProviderInstaller.cs
+++ Assets/Game/Scripts/Installers/GameConfigDataProviderInstaller.cs
@@ -3,14 +3,19 @@ using Game.Scripts.Services.GameDataProvider;
 using UnityEngine;
 using Zenject;
 
-namespace Game.Scripts
+namespace Game.Scripts.Installers
 {
-    public class GameConfigDataProviderInstaller : MonoInstaller
+    public class SceneInstaller : MonoInstaller
     {
         [SerializeField]
         private GameConfig _gameConfig;
 
         public override void InstallBindings()
+        {
+            BindGameConfigDataProvider();
+        }
+
+        private void BindGameConfigDataProvider()
         {
             IGameConfigDataProvider gameConfigDataProvider = new GameConfigDataProvider(_gameConfig);
             Container.Bind<IGameConfigDataProvider>().FromInstance(gameConfigDataProvider).AsSingle();
